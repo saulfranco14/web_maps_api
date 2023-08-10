65ccb8cd37e65c7b43902e9ce332ee288f4703b4
saulfranco14 <saul.franco1420@gmail.com>
2023-08-10 11:37:57 -0600

    se configura la bd para el problema de asynio

--- src/config/bd.py
+++ src/config/bd.py
@@ -1,9 +1,11 @@
-import motor.motor_asyncio
-import os
-from decouple import config
+import  motor.motor_asyncio
+import  os
+from    decouple            import config
 
-async def Database(table: str) -> motor.motor_asyncio.AsyncIOMotorCollection:
-    MONGO_DETAILS = config("MONGODB_URL")
-    client = motor.motor_asyncio.AsyncIOMotorClient(MONGO_DETAILS)
-    database = client.web_maps
-    return database.get_collection(table)
+MONGO_DETAILS       = config("MONGODB_URL")
+client              = motor.motor_asyncio.AsyncIOMotorClient(MONGO_DETAILS)
+database            = client.pars_dev
+
+async def Database(table):
+    data_collection   = database.get_collection(table)
+    return data_collection

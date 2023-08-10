a1d3678d841a5f44674a4b182842cc866bbc2b83
saulfranco14 <saul.franco1420@gmail.com>
2023-08-10 11:30:09 -0600

    se configura para nueva version de python

--- src/config/bd.py
+++ src/config/bd.py
@@ -1,11 +1,9 @@
-import  motor.motor_asyncio
-import  os
-from    decouple            import config
+import motor.motor_asyncio
+import os
+from decouple import config
 
-MONGO_DETAILS       = config("MONGODB_URL")
-client              = motor.motor_asyncio.AsyncIOMotorClient(MONGO_DETAILS)
-database            = client.web_maps
-
-async def Database(table):
-    data_collection   = database.get_collection(table)
-    return data_collection
+async def Database(table: str) -> motor.motor_asyncio.AsyncIOMotorCollection:
+    MONGO_DETAILS = config("MONGODB_URL")
+    client = motor.motor_asyncio.AsyncIOMotorClient(MONGO_DETAILS)
+    database = client.web_maps
+    return database.get_collection(table)
